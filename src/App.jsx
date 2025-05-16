1d4a128ffae2cfd95501194eac754b8b92616c33
Kelvin Kipchumba <koechkelvin2001@gmail.com>
2025-05-16 13:25:12 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,21 @@
 import React, {useState} from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Navbar from './Homepage/navbar';
+import Navbar from './Homepage/navbar/navbar';
 import AdminPage from './Admin/adminPage';
 import Authentification from './Authentification/index';
 import './App.css';
 
 const App = () => {
-
-
   return (
-
-    <div>
-         <Router>
-      <Routes>
-        {/* <Route path="/*" element={<AdminPage />} /> */}
-<<<<<<< HEAD
-        <Route path="/*" element={<Authentification />} />
-=======
-        <Route path="/navbar" element={<navbar />} />
->>>>>>> ibrah
-      </Routes>
+      <Router>
+        <Routes>
+          {/* <Route path="/*" element={<AdminPage />} /> */}
+          {/* <Route path="/*" element={<Authentification />} /> */}
+          <Route path="/*" element={<Navbar />} />
+        </Routes>
     </Router>
-    </div>
   );
 }
 
 export default App;
-
-// should add backticks on the container upto to the word theme on line 12
