3fa2d55e6a0acec34b6e75ea0ad9eb4ba2f14d15
“ibrahimgithinji” <ibragithy@gmail.com>
2025-05-16 11:50:21 +0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Navbar from './Homepage/navbar';
 import AdminPage from './Admin/adminPage';
 
-function App() {
+const App = () => {
+
+
   return (
-    <Router>
+
+    <div>
+         <Router>
       <Routes>
-        <Route path="/*" element={<AdminPage />} />
+        {/* <Route path="/*" element={<AdminPage />} /> */}
+        <Route path="/navbar" element={<navbar />} />
       </Routes>
     </Router>
+    </div>
   );
 }
 
 export default App;
+
+// should add backticks on the container upto to the word theme on line 12
